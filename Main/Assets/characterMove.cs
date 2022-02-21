48b2d460b5dae21e57d03a1785d117c284fe94aa
Utsav KC <utsav.kc@mavs.uta.edu>
2022-02-21 12:27:12 -0600

    added animation to player movement

--- Main/Assets/characterMove.cs
+++ Main/Assets/characterMove.cs
@@ -1,19 +1,28 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class characterMove : MonoBehaviour
 {
-	public float moveSpeed;
+	public float moveSpeed = 5f ;
+     public Rigidbody2D rb;
+     Vector2 movement;
+     public Animator animator;
     // Start is called before the first frame update
-    void Start()
-    {
-         moveSpeed = 5f;
-    }
 
-    // Update is called once per frame
     void Update()
     {
-         transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime, 0f);
+          //transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime, 0f);
+          movement.x = Input.GetAxisRaw("Horizontal");
+          movement.y = Input.GetAxisRaw("Vertical");
+
+          animator.SetFloat("Horizontal",movement.x);
+          animator.SetFloat("Vertical",movement.y);
+          animator.SetFloat("Speed",movement.sqrMagnitude);  
     }
+
+    void FixedUpdate()
+    {
+         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+     }
 }
